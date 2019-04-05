124ba4b26e6b933569e1f56982a1bdac0c5bde69
locnt19 <locnt19@gmail.com>
2019-04-05 16:07:58 +0700

    add fixed-stuff, footer

--- src/shared/header/index.js
+++ src/shared/header/index.js
@@ -1,1 +1,25 @@
-// console.log('Header');
+$(function () {
+	$('.gis-header .toggle-menu').click(function () {
+		$(this).toggleClass('open')
+		$('.gis-header .middle-header-menu').toggleClass('open')
+	})
+
+	$("body").on("click", "a.ajaxpagerlink", function (e) {
+		e.preventDefault();
+		let obj = $(this);
+		let pageurl = $(this).attr('href');
+		if (!pageurl.length) return;
+
+		$.ajax({
+			url: pageurl,
+			data: {
+				isajax: true
+			},
+			success: function success(data) {
+				$('.ajaxresponse .ajaxresponsewrap').append($(data).find('.ajaxresponsewrap').html());
+				obj.remove();
+			}
+		});
+		return false;
+	});
+});
